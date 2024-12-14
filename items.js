2cfc4858f638425489e034e1f09ddda475bb529b
Dan Bretl <danbretl@gmail.com>
2024-12-14 03:38:25 -0800

    Change initial prompt and randomize the order of items

--- items.js
+++ items.js
@@ -1,13 +1,12 @@
-// Array of items (imported or defined inline)
 const items = [
   {
-    title: "Glorb",
-    description: "Programmable geometric lamp",
-    url: "https://glorb.me",
-    showAnimation: "images/GlorbLamp-show.mp4",
-    clearAnimation: "images/GlorbLamp-clear.mp4",
-    thumbnail: "images/GlorbLamp-thumbnail.jpg",
-    showDuration: 2910,
+    title: "Art book b&w",
+    description: "One of my favorite artists in black & white",
+    url: "https://shrigshop.com/collections/david-shrigley-books/products/david-shrigley-book-i-am-the-jug-you-are-the-glass",
+    showAnimation: "images/BookBW-show.mp4",
+    clearAnimation: "images/BookBW-clear.mp4",
+    thumbnail: "images/BookBW-thumbnail.jpg",
+    showDuration: 2510,
     clearDuration: 2710
   },
   {
@@ -21,56 +20,6 @@ const items = [
     clearDuration: 2610
   },
   {
-    title: "Ferrofluid sound visualizer",
-    description: "Desktop sound visualizer",
-    url: "https://asiro.store/products/ferrofluid-sound-visualizer-1",
-    showAnimation: "images/Ferrofluid-show.mp4",
-    clearAnimation: "images/Ferrofluid-clear.mp4",
-    thumbnail: "images/Ferrofluid-thumbnail.jpg",
-    showDuration: 2710,
-    clearDuration: 2710
-  },
-  {
-    title: "Rainbow moon sweatshirt",
-    description: "Glow in the dark sweatshirt in XL",
-    url: "https://www.phiosuwan.com/men/the-rainbow-moonvoice-glow-in-the-dark-crewneck-sweatshirt",
-    showAnimation: "images/MoonShirt-show.mp4",
-    clearAnimation: "images/MoonShirt-clear.mp4",
-    thumbnail: "images/MoonShirt-thumbnail.jpg",
-    showDuration: 2610,
-    clearDuration: 2610
-  },
-  {
-    title: "Piston fidget toy",
-    description: "Pneumatic fidgeting",
-    url: "https://www.metmo.co.uk/products/metmo-piston",
-    showAnimation: "images/PistonToy-show.mp4",
-    clearAnimation: "images/PistonToy-clear.mp4",
-    thumbnail: "images/PistonToy-thumbnail.jpg",
-    showDuration: 2810,
-    clearDuration: 2710
-  },
-  {
-    title: "Helico fidget toy",
-    description: "Rolly fidgeting",
-    url: "https://www.metmo.co.uk/products/helico?variant=42217707405473",
-    showAnimation: "images/HelicoToy-show.mp4",
-    clearAnimation: "images/HelicoToy-clear.mp4",
-    thumbnail: "images/HelicoToy-thumbnail.jpg",
-    showDuration: 2610,
-    clearDuration: 2610
-  },
-  {
-    title: "Cube fidget toy",
-    description: "Cubic fidgeting",
-    url: "https://www.metmo.co.uk/pages/the-cube-drop",
-    showAnimation: "images/SteelCube-show.mp4",
-    clearAnimation: "images/SteelCube-clear.mp4",
-    thumbnail: "images/SteelCube-thumbnail.jpg",
-    showDuration: 2610,
-    clearDuration: 2710
-  },
-  {
     title: "Jellyfish lamp",
     description: "Floating glowing jellyfish",
     url: "https://jellyflow.co/products/jellyfish-lamp-dancing-legs-rotating-tentacles-led-light",
@@ -81,22 +30,32 @@ const items = [
     clearDuration: 2710
   },
   {
-    title: "Sandelier",
-    description: "Sand art in Sahara (Orange), any size",
-    url: "https://sandelier.com/products/wall-mount-sandelier-black?variant=49366688661842",
-    showAnimation: "images/SandFrame-show.mp4",
-    clearAnimation: "images/SandFrame-clear.mp4",
-    thumbnail: "images/SandFrame-thumbnail.jpg",
-    showDuration: 2810,
+    title: "Glorb",
+    description: "Programmable geometric lamp",
+    url: "https://glorb.me",
+    showAnimation: "images/GlorbLamp-show.mp4",
+    clearAnimation: "images/GlorbLamp-clear.mp4",
+    thumbnail: "images/GlorbLamp-thumbnail.jpg",
+    showDuration: 2910,
     clearDuration: 2710
   },
   {
-    title: "Device desk mount",
-    description: "Desk mount for iPhone or iPad",
-    url: "https://cyber-vintage.com/products/supergrip-desk-mount-phone-tablet",
-    showAnimation: "images/DeskMount-show.mp4",
-    clearAnimation: "images/DeskMount-clear.mp4",
-    thumbnail: "images/DeskMount-thumbnail.jpg",
+    title: "Art book color",
+    description: "One of my favorite artists in color",
+    url: "https://shrigshop.com/collections/david-shrigley-books/products/david-shrigley-books-get-your-shit-together-2022",
+    showAnimation: "images/BookColor-show.mp4",
+    clearAnimation: "images/BookColor-clear.mp4",
+    thumbnail: "images/BookColor-thumbnail.jpg",
+    showDuration: 2810,
+    clearDuration: 2610
+  },
+  {
+    title: "Helico fidget toy",
+    description: "Rolly fidgeting",
+    url: "https://www.metmo.co.uk/products/helico?variant=42217707405473",
+    showAnimation: "images/HelicoToy-show.mp4",
+    clearAnimation: "images/HelicoToy-clear.mp4",
+    thumbnail: "images/HelicoToy-thumbnail.jpg",
     showDuration: 2610,
     clearDuration: 2610
   },
@@ -111,6 +70,66 @@ const items = [
     clearDuration: 2710
   },
   {
+    title: "Device desk mount",
+    description: "Desk mount for iPhone or iPad",
+    url: "https://cyber-vintage.com/products/supergrip-desk-mount-phone-tablet",
+    showAnimation: "images/DeskMount-show.mp4",
+    clearAnimation: "images/DeskMount-clear.mp4",
+    thumbnail: "images/DeskMount-thumbnail.jpg",
+    showDuration: 2610,
+    clearDuration: 2610
+  },
+  {
+    title: "Rainbow moon sweatshirt",
+    description: "Glow in the dark sweatshirt in XL",
+    url: "https://www.phiosuwan.com/men/the-rainbow-moonvoice-glow-in-the-dark-crewneck-sweatshirt",
+    showAnimation: "images/MoonShirt-show.mp4",
+    clearAnimation: "images/MoonShirt-clear.mp4",
+    thumbnail: "images/MoonShirt-thumbnail.jpg",
+    showDuration: 2610,
+    clearDuration: 2610
+  },
+  {
+    title: "Cube fidget toy",
+    description: "Cubic fidgeting",
+    url: "https://www.metmo.co.uk/pages/the-cube-drop",
+    showAnimation: "images/SteelCube-show.mp4",
+    clearAnimation: "images/SteelCube-clear.mp4",
+    thumbnail: "images/SteelCube-thumbnail.jpg",
+    showDuration: 2610,
+    clearDuration: 2710
+  },
+  {
+    title: "Piston fidget toy",
+    description: "Pneumatic fidgeting",
+    url: "https://www.metmo.co.uk/products/metmo-piston",
+    showAnimation: "images/PistonToy-show.mp4",
+    clearAnimation: "images/PistonToy-clear.mp4",
+    thumbnail: "images/PistonToy-thumbnail.jpg",
+    showDuration: 2810,
+    clearDuration: 2710
+  },
+  {
+    title: "Sandelier",
+    description: "Sand art in Sahara (Orange), any size",
+    url: "https://sandelier.com/products/wall-mount-sandelier-black?variant=49366688661842",
+    showAnimation: "images/SandFrame-show.mp4",
+    clearAnimation: "images/SandFrame-clear.mp4",
+    thumbnail: "images/SandFrame-thumbnail.jpg",
+    showDuration: 2810,
+    clearDuration: 2710
+  },
+  {
+    title: "Ferrofluid sound visualizer",
+    description: "Desktop sound visualizer",
+    url: "https://asiro.store/products/ferrofluid-sound-visualizer-1",
+    showAnimation: "images/Ferrofluid-show.mp4",
+    clearAnimation: "images/Ferrofluid-clear.mp4",
+    thumbnail: "images/Ferrofluid-thumbnail.jpg",
+    showDuration: 2710,
+    clearDuration: 2710
+  },
+  {
     title: "Pixel hoodie",
     description: "The most colorful hoodie in XL",
     url: "https://freshhoods.com/products/pied-hoodie?variant=40116247593037",
@@ -119,26 +138,6 @@ const items = [
     thumbnail: "images/PixelHood-thumbnail.jpg",
     showDuration: 2810,
     clearDuration: 2610
-  },
-  {
-    title: "Art book color",
-    description: "One of my favorite artists in color",
-    url: "https://shrigshop.com/collections/david-shrigley-books/products/david-shrigley-books-get-your-shit-together-2022",
-    showAnimation: "images/BookColor-show.mp4",
-    clearAnimation: "images/BookColor-clear.mp4",
-    thumbnail: "images/BookColor-thumbnail.jpg",
-    showDuration: 2810,
-    clearDuration: 2610
-  },
-  {
-    title: "Art book b&w",
-    description: "One of my favorite artists in black & white",
-    url: "https://shrigshop.com/collections/david-shrigley-books/products/david-shrigley-book-i-am-the-jug-you-are-the-glass",
-    showAnimation: "images/BookBW-show.mp4",
-    clearAnimation: "images/BookBW-clear.mp4",
-    thumbnail: "images/BookBW-thumbnail.jpg",
-    showDuration: 2510,
-    clearDuration: 2710
   },
   {
     title: "And lots and lots of surprises!",
